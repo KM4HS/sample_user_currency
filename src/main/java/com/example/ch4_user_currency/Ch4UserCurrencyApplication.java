7cd2816f1318fe289db02f649f467e532b905300
KM4HS <ac4hsu@gmail.com>
2024-11-28 13:22:30 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/ch4_user_currency/Ch4UserCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch4_user_currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4UserCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch4UserCurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ch4_user_currency/Ch4UserCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch4_user_currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch4UserCurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
